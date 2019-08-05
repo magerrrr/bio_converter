36ccb3193774d89e171500400e6e70e0a6e5fdec
Kirill Mager <kiruha24@mail.ru>
2019-08-05 22:27:48 +0300

    added new function converter
    new way to convert units

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+/*let converterDB = {
+	biomarkers: {
+		0: 'Insulin',
+		1: 'Glucose',
+		2: 'LDL'
+	},
+	units: {
+		0: 'mmol/l',
+		1: 'mg/dl'
+	}
+};
+*/
+function converter(biomarker, convertFrom, convertTo, valueToConvert){
+if (biomarker === 0){
+	if ((convertFrom === 0)&&(convertTo === 1)){
+		let result = valueToConvert*18
+		return result;
+	} else if ((convertFrom === 1)&&(convertTo === 0)) {
+		let result = valueToConvert/18
+		return result;
+	}
+	}
+} 
+
+
 function insulinConverter(insulinValueMMolL){
 	let insulinValueMgDl = insulinValueMMolL*18;
 	return insulinValueMgDl;
