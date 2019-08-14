e7c3b036116c16ad1faf64e25b22cf7761e1178c
Kirill Mager <kiruha24@mail.ru>
2019-08-14 17:01:54 +0300

    Added new biomarker Insulin

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 let converterDB = {
 	biomarkers: {
-		0: 'Insulin',
+		0: {name: 'Insulin',
+			unit:
+				[['mmol/l', 0],
+				 ['pmol/l', 6.945], // 14-140 нормальный диапазон
+				 ['mg/dl',  0.347], 
+ 				 ['mg/ml',  0.0347],
+ 				 ['g',      0.0000347],
+				 ['IU',     1], 
+				 ['µIU/mL', 1], // 2.0-20 нормальный диапазон
+				 ['mIU/L',  1] 
+				],
+			},
 		1: {name:'Glucose',
-			unit: [ ['mmol/l',1], ['mg/dl',18.01559] ],
+			unit: 
+				[['mmol/l', 1],
+				 ['pmol/l', 1000000000],
+				 ['mg/dl',  18.01559],
+				],
 			}
 	},
 	units: {
-		0: 'mmol/l',
-		1: 'mg/dl'
+		0: 'mmol/L',
+		1: 'pmol/L',
+		2: 'mg/dl',
+		3: 'mg/ml', // тоже самое что мг
+		4: 'g', // грамм
+		5: 'IU', // International Unit = МЕ (Международная Единица) = ЕД (Единица Действия). В фармакологии единица дозы вещества, основанная на его биологической активности
+		6: 'µIU/mL', // мкМЕ/мл = мкЕд/мл
+		7: 'mIU/L', // мМЕ/л
 	}
 };
 
 function converter(biomarker, convertFrom, convertTo, value){
-	//convert(1, 0, 1, 5) = 90.07795 mg/dl
-	//convert(1, 1, 0, 90.07795) = 5 mmol/l
-	//Проверяем аргументы, чтобы все были валидными
+	// Проверка для Инсулина: converter(0,1,6,180)       = 25.9 µIU/mL
+	// Проверка для Инсулина: converter(0,6,1,25)        = 173.6 pmol/L
+	// Проверка для Глюкозы:  convert(1, 0, 1, 5)        = 90.07795 mg/dl
+	// Проверка для Глюкозы:  convert(1, 1, 0, 90.07795) = 5 mmol/l
 	if(isNaN(biomarker)){
     	return "Ошибка! Значение аргумента 'biomarker' не числовое!\n Решение: Введите валидное значение аргумента 'biomarker'";
     	}  
@@ -34,7 +56,7 @@ function converter(biomarker, convertFrom, convertTo, value){
 	let convFrom = converterDB.biomarkers[biomarker].unit[convertFrom][1];
 	let convTo = converterDB.biomarkers[biomarker].unit[convertTo][1];
 	let result = value * convTo/convFrom;
-	
+
 	console.log('Результат(' + marker + '): ' + value + ' ' + unitFrom + ' = ' + result + ' ' + unitTo);
 	return result;
-} 
+}
