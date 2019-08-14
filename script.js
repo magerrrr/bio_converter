3f5b99d29c376c5928f8b245ace49d53b0bbc104
Kirill Mager <kiruha24@mail.ru>
2019-08-14 18:20:22 +0300

    Added new biomarker HDL
    + added toFixed method to result

--- script.js
+++ script.js
@@ -2,35 +2,47 @@ let converterDB = {
 	biomarkers: {
 		0: {name: 'Insulin',
 			unit:
-				[['mmol/L', 0],
-				 ['pmol/L', 6.945], // 14-140 нормальный диапазон
-				 ['mg/dL',  0.347], 
- 				 ['mg/mL',  0.0347],
+				[['mmol/L', 0        ],
+				 ['pmol/L', 6.945    ], // 14-140 нормальный диапазон
+				 ['mg/dL',  0.347    ], 
+ 				 ['mg/mL',  0.0347   ],
  				 ['g',      0.0000347],
-				 ['IU',     1], 
-				 ['µIU/mL', 1], // 2.0-20 нормальный диапазон
-				 ['mIU/L',  1], 
+				 ['IU',     1        ], 
+				 ['µIU/mL', 1        ], // 2.0-20 нормальный диапазон
+				 ['mIU/L',  1        ], 
 				],
 			},
 		1: {name:'Glucose',
 			unit: 
-				[['mmol/L', 1],
+				[['mmol/L', 1         ],
 				 ['pmol/L', 1000000000],
-				 ['mg/dL',  18.01559],
+				 ['mg/dL',  18.015588 ],
 				],
 			},
 		2: {name:'Cholesterol, low-density (LDL) (high level)',
 			unit: 
 				[['mmol/L', 0.0259], // > 4.144
-				 ['pmol/L', 0], 
-				 ['mg/dL',  1], // > 160
- 				 ['mg/mL',  0],
- 				 ['g',      0],
-				 ['IU',     0], 
-				 ['µIU/mL', 0], 
-				 ['mIU/L',  0], 
+				 ['pmol/L', 0     ], 
+				 ['mg/dL',  1     ], // > 160
+ 				 ['mg/mL',  0.1   ],
+ 				 ['g',      0     ],
+				 ['IU',     0     ], 
+				 ['µIU/mL', 0     ], 
+				 ['mIU/L',  0     ], 
 				],
-			},			
+			},
+		3: {name:'Cholesterol, high-density (HDL) (low level)',
+			unit: 
+				[['mmol/L', 0.0259], // < 1.03
+				 ['pmol/L', 0     ], 
+				 ['mg/dL',  1     ], // < 40
+ 				 ['mg/mL',  0.1   ],
+ 				 ['g',      0     ],
+				 ['IU',     0     ], 
+				 ['µIU/mL', 0     ], 
+				 ['mIU/L',  0     ], 
+				],
+			},						
 	},
 	units: {
 		0: 'mmol/L',
@@ -45,12 +57,6 @@ let converterDB = {
 };
 
 function converter(biomarker, convertFrom, convertTo, value){
-	// Проверка для Инсулина: converter(0,1,6,180) = 25.9 µIU/mL
-	// Проверка для Инсулина: converter(0,6,1,25)  = 173.6 pmol/L
-	// Проверка для  Глюкозы: convert(1,0,2,5)     = 90.07795 mg/dL
-	// Проверка для  Глюкозы: convert(1,1,0,90)    = 4.9 mmol/L
-	// Проверка для      LDL: convert(2,2,1,160)   = 4.14 mmol/L
-	// Проверка для      LDL: convert(2,1,0,90)    = 4.9 mmol/L
 	if(isNaN(biomarker)){
     	return "Ошибка! Значение аргумента 'biomarker' не числовое!\n Решение: Введите валидное значение аргумента 'biomarker'";
     	}  
@@ -71,6 +77,23 @@ function converter(biomarker, convertFrom, convertTo, value){
 	let convTo = converterDB.biomarkers[biomarker].unit[convertTo][1];
 	let result = value * convTo/convFrom;
 
-	console.log('Результат(' + marker + '): ' + value + ' ' + unitFrom + ' = ' + result + ' ' + unitTo);
-	return result;
+	console.log('Биомаркер: ' + marker + '\n' + value + ' ' + unitFrom + ' = ' + result + ' ' + unitTo);
+	
+	return result.toFixed(2);
 }
+
+	// Проверка для Инсулина: converter(0,1,6,180) = 25.9 µIU/mL
+	// Проверка для Инсулина: converter(0,6,1,25)  = 173.6 pmol/L
+	// Проверка для  Глюкозы: convert(1,0,2,5)     = 90.07795 mg/dL
+	// Проверка для  Глюкозы: convert(1,1,0,90)    = 4.9 mmol/L
+	// Проверка для      LDL: convert(2,2,0,160)   = 4.14 mmol/L
+	// Проверка для      LDL: convert(2,0,2,4)     = 154.4 mg/dL
+	// Проверка для      HDL: convert(3,2,0,40)    = 1.036 mmol/L
+	// Проверка для      HDL: convert(3,0,2,1)     = 38.6 mg/dL	
+	// Проверка для      HDL: convert(3,0,3,1)     = 3.861 mg/dL
+
+
+
+
+
+
