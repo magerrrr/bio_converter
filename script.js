dbe5d09c9a8e521d65889f1e0684d677bb855308
Kirill Mager <kiruha24@mail.ru>
2019-08-14 14:13:39 +0300

    Added new interface
    included:
    - new object converterDB
    - new function converter with not a number check
    - clear all garbage

--- script.js
+++ script.js
@@ -1,78 +1,40 @@
 let converterDB = {
 	biomarkers: {
 		0: 'Insulin',
-		1: 'Glucose',
+		1: {name:'Glucose',
+			unit: [ ['mmol/l',1], ['mg/dl',18.01559] ],
+			}
 	},
-	units_from: {
-		0: 'mmol/l',
-		1: 'mg/dl'
-	},
-	units_to: {
+	units: {
 		0: 'mmol/l',
 		1: 'mg/dl'
 	}
 };
 
-//convert(1, 0, 1, 5)
 function converter(biomarker, convertFrom, convertTo, value){
+	//convert(1, 0, 1, 5) = 90.07795 mg/dl
+	//convert(1, 1, 0, 90.07795) = 5 mmol/l
+	//Проверяем аргументы, чтобы все были валидными
 	if(isNaN(biomarker)){
     	return "Ошибка! Значение аргумента 'biomarker' не числовое!\n Решение: Введите валидное значение аргумента 'biomarker'";
     	}  
 	if(isNaN(convertFrom)){
     	return "Ошибка! Значение аргумента 'convertFrom' не числовое!\n Решение: Введите валидное значение аргумента 'convertFrom'";
- 	}
+ 		}
 	if(isNaN(convertTo)){
     	return "Ошибка! Значение аргумента 'convertTo' не числовое!\n Решение: Введите валидное значение аргумента 'convertTo'";
- 	}  	 
+ 		}  	 
 	if(isNaN(value)){
     	return "Ошибка! Значение аргумента 'value' не числовое!\n Решение: Введите валидное значение аргумента 'value'";
- 	} 
+ 		} 
 
-	let marker = converterDB.biomarkers[biomarker];
-	let convFrom = converterDB.units_from[convertFrom];
-	let convTo = converterDB.units_to[convertTo];
+	let marker = converterDB.biomarkers[biomarker].name;
+	let unitFrom = converterDB.biomarkers[biomarker].unit[convertFrom][0]
+	let unitTo = converterDB.biomarkers[biomarker].unit[convertTo][0]
+	let convFrom = converterDB.biomarkers[biomarker].unit[convertFrom][1];
+	let convTo = converterDB.biomarkers[biomarker].unit[convertTo][1];
+	let result = value * convTo/convFrom;
 	
-	//return biomarker + ": " + value * convFrom / convTo;
-	console.log("Мы ввели biomarker: " + biomarker)
-	console.log("Мы ввели convertFrom: " + convertFrom)
-	console.log("Мы ввели convertTo: " + convertTo)
-	console.log("Мы ввели value: " + value)
-	console.log("-----------------------------------------")
-	console.log("Мы считаем " + marker + ". Переводим " + value + " " + convFrom + " в " + convTo )
+	console.log('Результат(' + marker + '): ' + value + ' ' + unitFrom + ' = ' + result + ' ' + unitTo);
+	return result;
 } 
-
-
-/*var Glucose_metrics = {
-  "mmol/l" : 1,
-  "mg/dl" : 18,01559
-};
-
-function convert(){
-  var val = document.getElementById("fromVal").value;
-  if(isNaN(val)){
-    return;
-  }  
-  <!-- если проверка прошла успешно, выполняем преобразование -->
-  document.getElementById("toVal").value = val * metrics[document.getElementById("fromSystem").value] /
-     metrics[document.getElementById("toSystem").value];
-}
-
-
-
-
-function insulinConverter(insulinValueMMolL) {
-  document.getElementById("outputMGPERDL").innerHTML=insulinValueMMolL*18.0156;
-}
-
-function converter(biomarker, convertFrom, convertTo, valueToConvert){
-	if (biomarker === 0){
-		if ((convertFrom === 0)&&(convertTo === 1)){
-			let result = valueToConvert*18.0156
-			return result;
-			} else if ((convertFrom === 1)&&(convertTo === 0)) {
-			let result = valueToConvert/18.0156
-			return result;
-		}
-	}
-} 
-*/
